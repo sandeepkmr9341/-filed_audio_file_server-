799c28f4d74f9e7f29b859f77eaa5fd59b2b1a7a
sandeep parida <62209723+sandeepkmr9341@users.noreply.github.com>
2021-05-10 12:20:42 +0530

    Delete application directory

--- application/audio/song.py
+++
@@ -1,7 +0,0 @@
-from application.audio import BaseAudio
-from application.databases import db, Column
-
-
-class Song(BaseAudio):
-    __tablename__ = "song"
-    name = Column(db.String(100), nullable=False)
--- application/extensions.py
+++
@@ -1,7 +0,0 @@
-"""Extensions module. Each extension is initialized in the application factory located in application.py."""
-
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-migrate = Migrate()
